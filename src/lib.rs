5a461ecf050bfd46f248f574b02046675b5473c2
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-06-13 14:56:14 -0400

    src/lib.rs:print switch

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 /// ###### count_set_bits
 ///
 /// ```
-/// pub fn count_set_bits(x: u128) -> u128 {
+/// pub fn count_set_bits(x: u128, print: bool) -> u128 {
 ///     let mut count = 0;
 ///     let mut temp = x;
 ///
@@ -26,22 +26,22 @@
 /// use count_set_bits::count_set_bits;
 /// fn main() {
 ///     let num = 13; // Example usage, change x as needed
-///     let set_bits = count_set_bits(num.try_into().unwrap());
+///     let set_bits = count_set_bits(num.try_into().unwrap(), true);
 ///
 ///     println!("\nNumber of set bits in {}: {}", num, set_bits);
 /// }
 /// ```
 ///
 
-pub fn count_set_bits(x: u128) -> u128 {
+pub fn count_set_bits(x: u128, print: bool) -> u128 {
     let mut count = 0;
     let mut temp = x;
 
     // Iterate through each bit position (power of 2)
     while temp > 0 {
-        //if cfg!(debug_assertions) {
-        print!("{}", temp & 1);
-        //}
+        if print {
+            print!("{}", temp & 1);
+        }
         count += temp & 1;
         temp >>= 1;
     }
@@ -55,12 +55,12 @@ mod tests {
     use super::*;
     #[test]
     fn cs_bits_64() {
-        let result: u128 = count_set_bits(111111111111111111111111111111111111111);
+        let result: u128 = count_set_bits(111111111111111111111111111111111111111, true);
         assert_eq!(result, 64);
     }
     #[test]
     fn cs_bits_128() {
-        let result: u128 = count_set_bits(340282366920938463463374607431768211455);
+        let result: u128 = count_set_bits(340282366920938463463374607431768211455, true);
         assert_eq!(result, 128);
     }
 }
